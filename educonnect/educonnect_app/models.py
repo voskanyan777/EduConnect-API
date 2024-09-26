35dc279df71eb9c46c93409ea4df3521d2878368
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-09-26 18:27:29 +0700

    Metaclass added for all models.

--- educonnect/educonnect_app/models.py
+++ educonnect/educonnect_app/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
@@ -8,7 +8,25 @@ class User(AbstractUser):
         ("teacher", "Teacher"),
     ]
     role = models.CharField(max_length=7, choices=ROLE_CHOICES, default="student")
-    group_number = models.CharField(max_length="25")
+    group_number = models.CharField(max_length=25)
+
+    # Добавляем related_name для полей groups и user_permissions
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",  # Уникальное имя для обратной связи
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",  # Уникальное имя для обратной связи
+        blank=True
+    )
+
+    class Meta:
+        db_table = 'Users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
 
 
 class Course(models.Model):
@@ -17,6 +35,11 @@ class Course(models.Model):
     available_groups = models.JSONField()
     created_by_teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
+    class Meta:
+        db_table = 'Courses'
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
 
 class Task(models.Model):
     name = models.CharField(max_length=90, null=False)
@@ -24,8 +47,18 @@ class Task(models.Model):
     for_course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False)
     created_by_teacher = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
+    class Meta:
+        db_table = 'Tasks'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
 
 class Group(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     group_number = models.CharField(max_length=25, null=False)
     students_count = models.IntegerField()
+
+    class Meta:
+        db_table = 'Groups'
+        verbose_name = 'Group'
+        verbose_name_plural = 'Groups'
